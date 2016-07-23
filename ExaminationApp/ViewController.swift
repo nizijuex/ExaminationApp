1a8a425fda40760a559407502f0b7f49bd69024d
Student <student@m303-22.local>
2016-07-23 12:04:33 +0900

    nizijuex

--- ExaminationApp/ViewController.swift
+++ ExaminationApp/ViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate {
+    
+    @IBOutlet weak var hidari:UITextField!
+    @IBOutlet weak var migi:UITextField!
+    @IBOutlet weak var fourArithmetic:UIPickerView!
+    @IBOutlet weak var result:UILabel!
+    
+    @IBAction func calculate(sender:AnyObject){
+        
+    }
     /// 演算子のリスト
     private let operatorList = [
         "+",
         "-",
+        "x",
+        
     ]
     
+    func numberOfComponentsInPikerView(pikerView: UIPickerView)->Int{
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInCompent component:Int) -> Int {
+        return operatorList.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row:Int,forComponent component:Int) -> String! {
+        return operatorList[row]
+    }
+    /*
     /// 選択中の演算子
-    private var selectedOperator = '+'
+    private let selectedOperator:Character = "+"
     
     /// 値1の入力フィールド
     @IBOutlet private var value1: UITextField!
@@ -25,8 +47,13 @@ class ViewController: UIViewController {
     
     /// 計算実行ボタン押下時の処理
     @IBAction private func calcurate(_: UIButton) {
-        let result = Int(value1.text!)! + Int(value2.text)
-        
+       
+        let numStr1:String = value1.text!
+        let numStr2:String = value2.text!
+        let num1:Int= (Int)numStr1
+        let num2:Int= (Int)numStr2
+        let sum:Int = num1 + num2
+        let result = sum
         // TODO: 計算結果ラベルの値を書き換えるようにする
         print("result: \(result)")
     }
@@ -54,4 +81,5 @@ extension ViewController: UIPickerViewDelegate {
         // 行を選択した時のアクションを定義
         // TODO: 選択した演算子で selectedOperator を上書きする
     }
+*/
 }
